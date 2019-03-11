edf4d0a68120c1eafb014bf9c8ff2bf6ec41d682
Meng <809218767@qq.com>
2019-03-11 19:50:16 +0800

    project init

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,15 +1,16 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+<div>
+  Hello World!
+</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'home'
+
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
