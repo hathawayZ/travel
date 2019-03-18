d82e1301d3cb4e9159d68be1050e5913a9dc4e16
Meng <809218767@qq.com>
2019-03-18 20:52:23 +0800

    add recommend

---
+++ src/pages/home/components/recommend.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <div class="title">
+      热销推荐
+    </div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of recommendList"
+        :key="item.id"
+      >
+        <img class="item-img" :src="item.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">
+            {{ item.title }}
+          </p>
+          <p class="item-desc">
+            {{ item.desc }}
+          </p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+          title: '上海迪士尼',
+          desc: '迪士尼乐园还原你的公主梦'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+          title: '上海迪士尼',
+          desc: '迪士尼乐园还原你的公主梦'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+          title: '上海迪士尼',
+          desc: '迪士尼乐园还原你的公主梦'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  line-height :.8rem
+  background :#eee
+  margin-top:.2rem
+  text-indent :.2rem
+.item
+  display :flex
+  height :1.9rem
+  overflow:hidden
+  .item-img
+    width :1.7rem
+    height :1.7rem
+    padding :.1rem
+  .item-info
+    flex:1
+    min-width :0
+    padding :.1rem
+    .item-title
+      line-height :.54rem
+      height :.54rem
+      font-size :.32rem
+      ellipsis()
+    .item-desc
+      line-height :.4rem
+      color:#ccc
+      ellipsis()
+    .item-button
+      padding :0 .2rem
+      background :#ff9300
+      line-height :.44rem
+      margin-top :.16rem
+      border-radius :.06rem
+      color :#ffffff
+</style>
